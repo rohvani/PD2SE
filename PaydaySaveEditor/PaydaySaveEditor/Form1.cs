06762a15b6e9065ffab68c609f966633882155e1
Seefo <rezen1911@gmail.com>
2013-07-29 22:32:28 -0400

    Encryption now works, huzzah! Commiting before I break anything...
    Going to clean up the code and stuff now that encryption works.

--- PaydaySaveEditor/PaydaySaveEditor/Form1.cs
+++ PaydaySaveEditor/PaydaySaveEditor/Form1.cs
@@ -19,20 +19,25 @@ namespace PaydaySaveEditor
 		}
 
 		byte[] xorKey = { 0x5E, 0x2E, 0x23, 0x56, 0x25, 0x33, 0x34, 0x39, 0x35, 0x32, 0xF3, 0x32, 0x64, 0xB4, 0x33, 0x25, 0xA4, 0x48, 0xA4, 0x64, 0x32, 0x63, 0x32, 0x63, 0x20, 0x6C, 0x4D, 0x3C };
-		FileStream saveFileStream;
+		MemoryStream saveFileStream = new MemoryStream();
 
 		private void btnLoadSave_Click(object sender, EventArgs e)
 		{
-			Stream saveStream;
 			if (openSaveFileDialog.ShowDialog() == DialogResult.OK)
 			{
 				try
 				{
-					if ((saveStream = openSaveFileDialog.OpenFile()) != null)
+					if ((openSaveFileDialog.OpenFile()) != null)
 					{
-						saveStream = new FileStream(openSaveFileDialog.FileName, FileMode.Open, FileAccess.Read);
-						transform((FileStream)saveStream, false, "");
-						saveStream.Close();
+						using (FileStream fileStream = File.OpenRead(openSaveFileDialog.FileName))
+						{
+
+							MemoryStream ms = new MemoryStream();
+							transformTest(fileStream, saveFileStream);
+
+							// saveFileStream.Position = 0;
+							// transformTest(saveFileStream, File.Create("temp_encrypted.dat"));
+						}
 					}
 				}
 				catch (Exception ex)
@@ -48,7 +53,8 @@ namespace PaydaySaveEditor
 			{
 				try
 				{
-					transform(saveFileStream, true, saveFileDialog.FileName);
+					saveFileStream.Position = 0;
+					transformTest(saveFileStream, File.Create(saveFileDialog.FileName));
 				}
 				catch (Exception ex)
 				{
@@ -59,7 +65,7 @@ namespace PaydaySaveEditor
 
 		public void transform(FileStream file, bool save, String filePath)
 		{
-			byte[] data = new byte[(file.Length - 16)]; // minus 16 because we need to remove the old MD5 footer
+			byte[] data = new byte[file.Length];
 			file.Read(data, 0, data.Length);
 
 			int xorOffset;
@@ -76,16 +82,32 @@ namespace PaydaySaveEditor
 
 				FileStream newFile = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
 
-				newFile.Write(data, 0, data.Length);
+				newFile.Write(data, 0, (data.Length - 16));
 				newFile.Write(md5, 0, md5.Length);
 
 				newFile.Close();
 			}
 			else
 			{
-				saveFileStream = new FileStream(Application.StartupPath + "/temp.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-				saveFileStream.Write(data, 0, data.Length);
+				//saveFileStream = new FileStream(Application.StartupPath + "/temp.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+				//saveFileStream.Write(data, 0, data.Length);
 			}
+		}
+	
+		public void transformTest(Stream input, Stream output)
+		{
+			byte[] data = new byte[input.Length];
+			input.Read(data, 0, data.Length);
+
+			int xorOffset;
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				xorOffset = (i % xorKey.Length);
+				data[i] ^= xorKey[xorOffset];
+			}
+
+			output.Write(data, 0, data.Length);
 		}
 	}
 }
